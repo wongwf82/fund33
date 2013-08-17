0e33a321019940d0ec2a513bf4e1daf112f43173
Fai Wong <wongwf82@gmail.com>
2013-08-17 17:56:13 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fund33::Application.config.secret_token = 'f705e878488eda9d296cb66840f96422cefb5cbc9aa49ddad932eaecb220660808dde13a1f6ae31cca0996767e4230f893a17676c6acdeae501ba3d0e0da5596'
