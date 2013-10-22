0b30ec522f130dfeb8a75a2dd3484342b35bb806
Fai Wong <wongwf82@gmail.com>
2013-10-23 02:04:24 +0800

    Remove email uniqueness check

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,9 @@ class User < ActiveRecord::Base
   				  	:deck, :if => :is_updating_user?
   validates_attachment_size :deck, :less_than => 2.megabytes, :message => 'should be less than 2MB.'
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
-  					uniqueness: { case_sensitive: false }
+#  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+#  					uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
   private
 
